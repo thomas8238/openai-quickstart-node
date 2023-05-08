5f040d142959e616fa878d064eaf1e53a6d5f325
thomas8238 <90787151+thomas8238@users.noreply.github.com>
2023-05-07 19:14:26 -0500

    finished

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,27 @@
+import React from "react";
 import Head from "next/head";
 import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [player, setPlayer] = useState("");
   const [result, setResult] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
+    setResult('');
     try {
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ animal: animalInput }),
+        body: JSON.stringify({ player: player }),
       });
 
       const data = await response.json();
-      if (response.status !== 200) {
-        throw data.error || new Error(`Request failed with status ${response.status}`);
-      }
-
-      setResult(data.result);
-      setAnimalInput("");
+      setResult(data.result.replaceAll('\\n', '<br />'));
+      setPlayer("");
     } catch(error) {
       // Consider implementing your own error handling logic here
       console.error(error);
@@ -35,23 +33,24 @@ export default function Home() {
     <div>
       <Head>
         <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <h3>Analyze Player</h3>
         <form onSubmit={onSubmit}>
           <input
             type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+            name="player"
+            placeholder="Enter an player"
+            value={player}
+            onChange={(e) => setPlayer(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Generate Analysis" />
         </form>
-        <div className={styles.result}>{result}</div>
+        <div
+         className={styles.result}
+         dangerouslySetInnerHTML={{ __html: result }}
+        />
       </main>
     </div>
   );
